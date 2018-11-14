73f588ca5e4e662e626de0f574e7aeec4b019dcf
Tanja Šarčević <tanja.sarcevic@fer.hr>
2018-11-14 08:55:52 +0100

    Config files for marital-status different emph-race runs

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ def main():
      #   config_file = "config/config_" + str(k)
       #  sangria.run(config_file)
     # sangria.run("../config/config_test")
-    sangria.run("./config/config_7")
-    sangria.run("./config/config_8")
-    sangria.run("./config/config_9")
+    sangria.run("./config/config_30")
 
 
 if __name__ == '__main__':
