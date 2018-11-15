4b2fac7507ec1582c7511b0f54e8f39e2f525eef
tanjascats <ta.sarcavic@gmail.com>
2018-11-15 10:35:29 +0100

    Anonymized target edu-num weights on age finished

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import src.SaNGreeA as sangria
 
 def main():
     # here goes definition of all config files and loop that runs the algorithm for each of them
-    for k in range(40, 50):
-        config_file = "config/config_" + str(k)
-        sangria.run(config_file)
+    #for k in range(40, 50):
+     #   config_file = "config/config_" + str(k)
+      #  sangria.run(config_file)
     # sangria.run("../config/config_test")
-    #sangria.run("./config/config_28")
+    sangria.run("./config/config_39")
 
 
 if __name__ == '__main__':
